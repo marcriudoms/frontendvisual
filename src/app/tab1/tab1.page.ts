8e9ee917af1185359187c36599d84e1231d1af7b
Fihoca12 <Fihoca12@SINENSIA.EDU>
2019-12-17 16:25:15 +0100

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var google;
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,5 +10,58 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+
+  showChartSuenoMiDia () {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Sueño profundo', 3],
+      ['Sueño ligero', 1],
+      
+    ]);
+  
+    // Set chart options
+    var options = {'Sueño profundo':4,
+                   'Sueño ligero':3},
+    pieHole: 0.4;
+  
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_SuenoMiDia'));
+        chart.draw(data, options);
+   
+    }
+
+
+
+
+ /*
+    
+  showChartSuenoMiDia () {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Sueño');
+    data.addColumn('number', 'Horas');
+    data.addRows([
+      ['Sueño ligero', 3],
+      ['Sueño profundo', 4],
+    ]);
+  
+    // Set chart options
+    var options = {
+                   'Sueño profundo':4,
+                   'Sueño ligero':3};
+  
+    // Instantiate and draw our chart, passing in some options.
+    
+    var chart = new google.visualization.PieChart(document.getElementById('chart_SuenoMiDia'));
+    chart.draw(data, options);
+    */
+  
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var google;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -8,5 +10,79 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+
+showChart () {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
+
+  // Set chart options
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 'width':400,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+  }
+
+  showChart2 () {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+  
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
+  
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
+    chart.draw(data, options);
+    }
+
+    showChart3 () {
+
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+      ]);
+    
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+                     'width':400,
+                     'height':300};
+    
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.Histogram(document.getElementById('chart_div3'));
+      chart.draw(data, options);
+      }
+
+
 
 }
